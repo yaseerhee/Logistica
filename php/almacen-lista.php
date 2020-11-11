02da05f845fd86ef02f72d633a87c360e14aec61
Yaser Haddad <mohamedyaserhaddad@gmail.com>
2020-11-11 16:13:52 +0100

    Fichero listado almacen e imgs

--- php/almacen-lista.php
+++ php/almacen-lista.php
@@ -1,3 +1,46 @@
 <?php
 
-require_once "_varios.php";
+require_once "__varios.php";
+
+$pdo = obtenerPdoConexionBD();
+$sql = "SELECT id,nombre,Lugar FROM almacen ORDER BY id";
+
+$select = $pdo->prepare($sql);
+$select->execute([]);
+$rs = $select->fetchAll();
+?>
+
+<!DOCTYPE html>
+
+<head>
+    <meta charset="UTF-8">
+</head>
+
+<body>
+    <div class="contenedor">
+        <h1 id="listado">LISTADO DE ALMACENES</h1>
+
+        <table>
+            <tr>
+                <th>NOMBRE</th>
+                <th>LUGAR</th>
+                <th>ELIMINAR</th>
+            </tr>
+
+            <?php foreach ($rs as $fila) { ?>
+                <tr>
+                    <td><a href="almacen-ficha.php?id=<?= $fila["id"] ?>" <?= $fila["nombre"] ?>></a></td>
+                    <td><a href="almacen-ficha.php?id=<?= $fila["id"] ?>" <?= $fila["lugar"] ?>></a></td>
+                    <td><a href="almacen-eliminar.php?id=<?= $fila["id"] ?>">
+                            <img src="/img/img_X.png" width="25px" height="25px"> </a></td>
+                </tr>
+            <?php } ?>
+        </table>
+
+        <br />
+        <a href="almacen-ficha.php?id=-1">Introducir nuevo almacen</a>
+        <a href="productos-lista.php">Gestionar listado de productos</a>
+    </div>
+</body>
+
+</html>
